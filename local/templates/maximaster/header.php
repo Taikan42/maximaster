5726259f31ef050725363b58d2081c1af0990963
Anton <a.stankevich@maximaster.ru>
2016-06-30 13:03:22 +0300

    Components

--- local/templates/maximaster/header.php
+++ local/templates/maximaster/header.php
@@ -14,6 +14,24 @@
         <p>Приветствие</p>
         <div class="mobile-icon">
             <div class="navigate navigate-static" id="head-navigate">
+
+                <?$APPLICATION->IncludeComponent(
+                    "bitrix:menu",
+                    "",
+                    Array(
+                        "ALLOW_MULTI_SELECT" => "N",
+                        "CHILD_MENU_TYPE" => "left",
+                        "DELAY" => "N",
+                        "MAX_LEVEL" => "1",
+                        "MENU_CACHE_GET_VARS" => array(""),
+                        "MENU_CACHE_TIME" => "3600",
+                        "MENU_CACHE_TYPE" => "N",
+                        "MENU_CACHE_USE_GROUPS" => "Y",
+                        "ROOT_MENU_TYPE" => "top",
+                        "USE_EXT" => "N"
+                    )
+                );?>
+                <?/*
                 <nav>
                     <ul>
                         <li class="current"><a href="#">Главная</a></li>
@@ -21,13 +39,14 @@
                         <li><a href="#">Контакты</a></li>
                     </ul>
                 </nav>
+                */?>
             </div>
         </div>
     </header>
     <section class="flex-container">
         <div class="flex-block left-block">
             <h2>Левый сайд бар-меню</h2>
-            <?$APPLICATION->IncludeComponent("bitrix:menu", "catalog_horizontal", array(
+            <?/*$APPLICATION->IncludeComponent("bitrix:menu", "catalog_horizontal", array(
                 "ROOT_MENU_TYPE" => "left",
                 "MENU_CACHE_TYPE" => "A",
                 "MENU_CACHE_TIME" => "36000000",
@@ -43,6 +62,6 @@
                 "ALLOW_MULTI_SELECT" => "N",
             ),
                 false
-            );?> </div>
+            );*/?> </div>
         <div class="flex-block">
             <h1><?=$APPLICATION->ShowTitle(false);?></h1>
