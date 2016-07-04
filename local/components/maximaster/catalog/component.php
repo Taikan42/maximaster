d77d27d15157de7109c110305dc28e3f7f4e4f30
Anton <a.stankevich@maximaster.ru>
2016-07-04 15:06:14 +0300

    Cоздание компонента catalog v0.5-работ.

--- local/components/maximaster/catalog/component.php
+++ local/components/maximaster/catalog/component.php
@@ -1,16 +1,43 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-/*$Select = Array("IBLOCK_SECTION_ID", "NAME");
-$arFilter = Array("IBLOCK_ID" => 4);
-$res = CIBlockSection::GetList(
-    Array(),
-    $arFilter,
-    false,
-    $Select,
-    false
-);
-while ($ob = $res->GetNextElement()) {
-    $arFields = $ob->GetFields();
-    print_r($arFields);
+ini_set('display_errors', 1);
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+if(CModule::IncludeModule("iblock")) {
+    $idSECTION = $_GET["SECTION_ID"];
+    $arResult["SECTION"] = array();
+    $arResult["ELEMENT"] = array();
+    $res = CIBlockSection::GetByID($idSECTION);
+    if ($ar_res = $res->GetNext()) {
+        $arResult["SECTION"]["NAME"] = $ar_res['NAME'];
+        $arResult["SECTION"]["DESCRIPTION"] = $ar_res['DESCRIPTION'];
+        $arResult["SECTION"]["PICTURE"] = $ar_res['PICTURE'];
+    }
+    $arSelect = Array(
+        "ID",
+        "IBLOCK_ID",
+        "IBLOCK_SECTION_ID",
+        "NAME",
+        "PREVIEW_TEXT",
+        "PREVIEW_PICTURE",
+        "PROPERTY_PRICE",
+    );//IBLOCK_ID и ID обязательно должны быть указаны, см. описание arSelectFields выше
+    $arFilter = Array(
+        "SECTION_ID" => $idSECTION,
+        "INCLUDE_SUBSECTIONS" => "Y"
+    );
+    $res = CIBlockElement::GetList(
+        Array(),
+        $arFilter,
+        false,
+        false,
+        $arSelect);
+    while ($ob = $res->GetNextElement()) {
+        $arFields = $ob->GetFields();
+        $arResult["ELEMENT"][] = array(
+            "NAME" => $arFields["NAME"],
+            "PREVIEW_TEXT" => $arFields["PREVIEW_TEXT"],
+            "PREVIEW_PICTURE" => $arFields["PREVIEW_PICTURE"],
+            "PRICE" => $arFields["PROPERTY_PRICE_VALUE"],
+        );
+    }
 }
-$arResult['DATE'] = date($arParams["TEMPLATE_FOR_DATE"]);*/
 $this->IncludeComponentTemplate();
--- local/components/maximaster/catalog/templates/.default/template.php
+++ local/components/maximaster/catalog/templates/.default/template.php
@@ -1,14 +1,27 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<p><?=$arResult['DATE']?></p>
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+$this->addExternalCss("/local/styles.css");?>
+<?if (!empty($arResult)):?>
 <section class="catalog">
     <div class="flex-cont">
-        <?/*foreach*/?>
-        <div class="flex-block">
-            <img src="" alt="">
-            <div class="content">
-                <header class="title"></header>
-                <p></p>
-            </div>
+        <div class="flex-block left-block">
+            <? echo CFile::ShowImage($arResult["SECTION"]["PICTURE"], 200, 200, "border=0", "", true); ?>
+        </div>
+        <div class="flex-block right-block">
+            <h3 class="title"><?echo $arResult["SECTION"]["NAME"]?></h3>
+            <p><?echo $arResult["SECTION"]["DESCRIPTION"]?></p>
         </div>
     </div>
+    <?foreach($arResult["ELEMENT"] as $arItem):?>
+        <div class="flex-cont">
+            <div class="flex-block left-block">
+                <? echo CFile::ShowImage($arItem["PREVIEW_PICTURE"], 100, 100, "border=0", "", true); ?>
+            </div>
+            <div class="flex-block right-block">
+                <h3 class="title"><?echo $arItem["NAME"]?></h3>
+                <p><?echo $arItem["PRICE"]?></p>
+                <p><?echo $arItem["PREVIEW_TEXT"]?></p>
+            </div>
+        </div>
+    <?endforeach?>
 </section>
+<?endif?>
