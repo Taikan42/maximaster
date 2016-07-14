5647558b5797e673503b5655b08d6e18f6da835f
Anton <a.stankevich@maximaster.ru>
2016-07-14 13:33:31 +0300

    Добавление кнопки покупки на странице разделов

--- catalog/List.php
+++ catalog/List.php
@@ -22,7 +22,7 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
     )
 );?>
 <? $APPLICATION->IncludeComponent("maximaster:catalog", ".default",
-    array(),
+    array("IBLOCK_ID" => "4"),
     false
 ); ?>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- local/components/maximaster/catalog/templates/.default/template.php
+++ local/components/maximaster/catalog/templates/.default/template.php
@@ -1,33 +1,63 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-$this->addExternalCss("/local/styles.css");
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 $APPLICATION->SetTitle($arResult["SECTION"]["NAME"]);
 ?>
-<?if (!empty($arResult)):?>
-<section class="catalog">
-    <?if ($arResult["CHECK_SECTION"]):?>
-        <div class="flex-cont section">
-            <div class="flex-block left-block">
-                <? echo CFile::ShowImage($arResult["SECTION"]["PICTURE"], 200, 200, "border=0", "", false); ?>
+<? if (!empty($arResult)): ?>
+    <section class="catalog">
+        <? if ($arResult["CHECK_SECTION"]): ?>
+            <div class="flex-cont section">
+                <div class="flex-block left-block">
+                    <? echo CFile::ShowImage($arResult["SECTION"]["PICTURE"], 200, 200, "border=0", "", false); ?>
+                </div>
+                <div class="flex-block right-block">
+                    <h3><? echo $arResult["SECTION"]["NAME"] ?></h3>
+                    <p><? echo $arResult["SECTION"]["DESCRIPTION"] ?></p>
+                </div>
             </div>
-            <div class="flex-block right-block">
-                <h3><?echo $arResult["SECTION"]["NAME"]?></h3>
-                <p><?echo $arResult["SECTION"]["DESCRIPTION"]?></p>
+        <? endif ?>
+        <? foreach ($arResult["ELEMENT"] as $arItem): ?>
+            <div class="flex-cont">
+                <div class="flex-block left-block">
+                    <a href="<? echo($arItem["DETAIL_PAGE_URL"]) ?>">
+                        <? echo CFile::ShowImage($arItem["PREVIEW_PICTURE"], 100, 100, "border=0", "", false); ?>
+                    </a>
+                </div>
+                <div class="flex-block right-block">
+                    <h3><? echo($arItem["NAME"]); ?></h3>
+                    <p class="price"><? echo $arItem["PRICE"] ?> <? echo $arItem["CURRENCY"] ?>.</p>
+                    <a href="#" class="buy_botton1" onclick="add2basket(<?echo $arItem["ID"]; ?>), PopUpShow()">в корзину</a>
+                    <p><? echo $arItem["PREVIEW_TEXT"] ?></p>
+                </div>
+            </div>
+        <? endforeach ?>
+        <div class="popup" onclick="PopUpHide()" id="popup1">
+            <div class="popup-content">
+                <a href="#" onclick="PopUpHide()"> Продолжить </a>
+                <a href="/local/templates/maximaster/cart/index.php">К корзине</a>
             </div>
         </div>
-    <?endif?>
-    <?foreach($arResult["ELEMENT"] as $arItem):?>
-        <div class="flex-cont">
-            <div class="flex-block left-block">
-                <a href="<?echo($arItem["DETAIL_PAGE_URL"]) ?>">
-                    <? echo CFile::ShowImage($arItem["PREVIEW_PICTURE"], 100, 100, "border=0", "", false); ?>
-                </a>
-            </div>
-            <div class="flex-block right-block">
-                <h3><? echo($arItem["NAME"]); ?></h3>
-                <p class="price"><? echo $arItem["PRICE"] ?> <?echo $arItem["CURRENCY"]?>.</p>
-                <p><? echo $arItem["PREVIEW_TEXT"] ?></p>
-            </div>
-        </div>
-    <?endforeach?>
-</section>
-<?endif?>
+    </section>
+<? endif ?>
+<script type="text/javascript">
+    $(document).ready(function(){
+        //Скрыть PopUp при загрузке страницы
+        PopUpHide();
+    });
+    //Функция отображения PopUp
+    function PopUpShow(){
+        $("#popup1").show();
+    }
+    //Функция скрытия PopUp
+    function PopUpHide(){
+        $("#popup1").hide();
+    }
+    function add2basket(ID)
+    {
+        var id = ID;
+        var clv = 1;
+        $.ajax({
+            type: 'POST',
+            url: "/local/templates/maximaster/cart/addbasket_ajax.php",
+            data: {id: ID, quantity: clv,}
+        });
+    }
+</script>
