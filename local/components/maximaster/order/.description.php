d22f5097df960f9c224f362cf710211a0fed3e1a
Anton <a.stankevich@maximaster.ru>
2016-07-18 16:30:08 +0300

    Создание формы оформления заказа

---
+++ local/components/maximaster/order/.description.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: STANKEVICH
+ * Date: 18.07.2016
+ * Time: 14:16
+ */
---
+++ local/components/maximaster/order/templates/.default/template.php
@@ -0,0 +1,75 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+$APPLICATION->SetTitle("Оформить заказ");
+?>
+<div class="form-order">
+    <form action="" class="order" id="sale_order">
+        <table>
+            <tr>
+                <td>
+                    <label for="name">Имя: </label>
+                </td>
+                <td>
+                    <input type="text" id="name"/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="surname">Фамилия: </label>
+                </td>
+                <td>
+                    <input type="text" id="surname"/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="middle_name">Отчество: </label>
+                </td>
+                <td>
+                    <input id="middle_name"/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="user_phone">Телефон: </label>
+                </td>
+                <td>
+                    <input type="text" id="user_phone" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="user_email">Email: </label>
+                </td>
+                <td>
+                    <input type="text" id="user_email">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <span>Доставка: </span>
+                </td>
+                <td>
+                    <select name="delivery">
+                        <option value="s1" selected>Доставка</option>
+                        <option value="s2" >Самовывоз</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <span>Способ оплаты: </span>
+                </td>
+                <td>
+                    <select name="payment_type">
+                        <option value="s1" >Наличный</option>
+                        <option value="s2" selected>Безналичный</option>
+                    </select>
+                </td>
+            </tr>
+        </table>
+    </form>
+</div>
+
+
+
+        
